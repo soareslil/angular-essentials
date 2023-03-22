427f159a8dda7104c3fb620c1294300f04353a0f
soareslil <lilianectd@gmail.com>
2023-03-22 15:12:11 -0300

    my first component

---
+++ src/app/user.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  template: `
+    <p> hello world</p>
+    <p> i am the user component </p>
+  `
+})
+
+export class UserComponent{}
